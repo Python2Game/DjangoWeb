a1b85a1cb2c7245ad3605cd6da34cbfb0c58a145
AraikaAbaevna <arailym.raimbek@gmail.com>
2020-12-02 20:46:44 +0500

    web

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField('Ваше имя', max_length=20)
+    title = models.CharField('Название услуги', max_length=40)
+
+
+
+    def __str__(self):
+        return self.name
