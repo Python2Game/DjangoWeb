05d4df51519631d6af3310d0e1783837717532c9
akmaralnassir <akmaralnassir@gmail.com>
2020-12-07 09:03:34 +0300

    web

--- bebeautiful/main/admin.py
+++ bebeautiful/main/admin.py
@@ -1,2 +1,9 @@
+from django.contrib import admin
+from .models import *
 
-
+admin.site.register(Servise)
+admin.site.register(Customer)
+admin.site.register(Apponitment)
+admin.site.register(Book_status)
+admin.site.register(User_status)
+admin.site.register(Booking_Paid)
