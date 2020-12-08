2b82742338f223e18c378dbac6d687891649bf46
AraikaAbaevna <arailym.raimbek@gmail.com>
2020-12-09 01:12:12 +0500

    web

--- bebeautiful/main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.forms import ModelForm, TextInput, DateTimeInput,Textarea
-
-class Register(models.Model):
-    title = models.CharField('Название услуги', max_length=40)
-    name = models.CharField('Ваше имя', max_length=20)
-    date= models.DateTimeField('Дата')
-    time = models.TextField('Время')
